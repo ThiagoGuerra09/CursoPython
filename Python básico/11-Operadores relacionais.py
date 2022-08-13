d4d04229311e4cc3a5852dd1746e224554dcfa27
Thiago Guerra <thiagogwf@gmail.com>
2022-08-13 10:57:17 -0300

    att

---
+++ Python básico/11-Operadores relacionais.py
@@ -0,0 +1,8 @@
+"""Operadores relacionais
+== > >= < <= !=
+
+
+
+"""
+# quando usamos um =, signifiaca atribuição de valores. Ex: valor=1
+# quando usamos == perguntamos se o valor é igual a true or false. Ex: if(valor==1)
