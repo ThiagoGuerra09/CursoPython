08b98533a2a32ad4be1376fd276df29548c15bff
Thiago Guerra <thiagogwf@gmail.com>
2022-08-16 20:04:42 -0300

    update

---
+++ Python básico/V-For in.py
@@ -0,0 +1,23 @@
+"""For in em Python
+Iterando strings com for
+Função range(start=0, stop, step=1)"""
+# texto = 'oi'
+# for n, letra in enumerate(texto):
+#     print(n, letra)
+
+#função range, primeiro passamos, o inicio, onde para e para pular de qunrto em quanto, ao não passar valores ela assume o padrão, começa em 0 e pula de 1 em 1
+
+# for numero in range(10):
+# #     print(numero)
+
+# # for numero in range(1,10,2):
+# #     print(numero)
+
+
+for numero in range(20,10,-1):
+    print(numero)
+# pode ser usada para achar multiplos
+for n in range(0,100,8):
+    print(n)
+#continue-pula para o proximo ciclo da repetição(laço)
+#break- sai da repetição/termina o laço
